a3454e1c3e24a459055b4c7ab7090cf89bcce822
autmwatl <noreply@companieshouse.gov.uk>
2023-08-21 12:03:06 +0100

    DVOP-2545 add cert config

--- groups/ecs/locals.tf
+++ groups/ecs/locals.tf
@@ -24,9 +24,13 @@ locals {
   automation_subnet_pattern = local.secrets.automation_subnet_pattern
   automation_vpc_pattern    = local.secrets.automation_vpc_pattern
 
-  certificate_arn             = lookup(local.secrets, "certificate_arn", null)
+  create_ssl_certificate      = var.ssl_certificate_name == "" ? true : false
+  ssl_certificate_arn         = var.ssl_certificate_name == "" ? aws_acm_certificate_validation.certificate[0].certificate_arn : data.aws_acm_certificate.certificate[0].arn
+
   dns_zone_name               = local.secrets.dns_zone_name
   load_balancer_dns_zone_name = local.secrets.load_balancer_dns_zone_name
 
+  image_owner_id = local.secrets.image_owner_id
+  grafana_image = "${local.image_owner_id}.dkr.ecr.${var.region}.amazonaws.com/${var.image_repository_name}:${var.grafana_image_version}"
 
 }
