f312c7929a87cc76ec4680dc7a1c2c027f5b070b
autmwatl <noreply@companieshouse.gov.uk>
2023-08-21 12:01:17 +0100

    DVOP-2545 add secrets, subnets, vpc, r53, cert

--- groups/ecs/data.tf
+++ groups/ecs/data.tf
@@ -1,4 +1,68 @@
-data "aws_ecr_image" "grafana_image" {
-  repository_name = "ci-grafana-image"
-  most_recent       = true
+data "vault_generic_secret" "secrets" {
+  path = "team-${var.team}/${var.account_name}/${var.region}/${var.environment}/${var.repository_name}"
+}
+
+data "vault_generic_secret" "account_ids" {
+  path = "aws-accounts/account-ids"
+}
+
+data "aws_vpc" "placement" {
+  filter {
+    name   = "tag:Name"
+    values = [local.placement_vpc_pattern]
+  }
+}
+
+data "aws_subnet" "placement" {
+  for_each = toset(data.aws_subnets.placement.ids)
+  id = each.value
+}
+
+
+data "aws_subnets" "placement" {
+  filter {
+    name = "vpc-id"
+    values = [data.aws_vpc.placement.id]
+  }
+
+  filter {
+    name   = "tag:Name"
+    values = [local.placement_subnet_pattern]
+  }
+}
+
+data "aws_vpc" "automation" {
+  filter {
+    name   = "tag:Name"
+    values = [local.automation_vpc_pattern]
+  }
+}
+
+data "aws_subnet" "automation" {
+  for_each = toset(data.aws_subnets.automation.ids)
+  id = each.value
+}
+
+data "aws_subnets" "automation" {
+  filter {
+    name = "vpc-id"
+    values = [data.aws_vpc.automation.id]
+  }
+
+  filter {
+    name   = "tag:Name"
+    values = [local.automation_subnet_pattern]
+  }
+}
+
+data "aws_route53_zone" "selected" {
+  name         = local.dns_zone_name
+  private_zone = false
+}
+
+data "aws_acm_certificate" "certificate" {
+  count       = local.create_ssl_certificate ? 0 : 1
+  domain      = var.ssl_certificate_name
+  statuses    = ["ISSUED"]
+  most_recent = true
 }
