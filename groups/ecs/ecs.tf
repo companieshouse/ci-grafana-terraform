4d8ff0200c9ea461cd9cc1cbe89d6ba24a8e030b
autmwatl <noreply@companieshouse.gov.uk>
2023-08-21 12:02:48 +0100

    DVOP-2545 amend image address, service network config

--- groups/ecs/ecs.tf
+++ groups/ecs/ecs.tf
@@ -16,7 +16,7 @@ resource "aws_ecs_task_definition" "grafana_task" {
   [
     {
       "name": "grafana-container",
-      "image": "${data.aws_ecr_image.grafana_image.most_recent}",
+      "image": "${local.image_owner_id}.dkr.ecr.${var.region}.amazonaws.com/${var.image_repository_name}:${var.grafana_image_version}",
       "essential": true,
       "portMappings": [
         {
@@ -34,11 +34,14 @@ resource "aws_ecs_service" "grafana_service" {
   cluster         = aws_ecs_cluster.grafana_cluster.id
   task_definition = aws_ecs_task_definition.grafana_task.arn
   launch_type     = "FARGATE"
+  depends_on = [
+    aws_iam_role.ecs_execution_role,
+  ]
+  desired_count = 1
 
   network_configuration {
-    subnets = ["subnet-abcde012", "subnet-bcde012a"]
-    assign_public_ip = true
+    subnets = local.placement_subnet_cidrs
+    assign_public_ip = false
   }
 
-  desired_count = 1
 }
