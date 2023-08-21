a1a294a826491631206e2733116c149b00e2ba31
autmwatl <noreply@companieshouse.gov.uk>
2023-08-21 12:03:02 +0100

    DVOP-2545 add cert config

--- groups/ecs/lb.tf
+++ groups/ecs/lb.tf
@@ -4,13 +4,14 @@ resource "aws_lb" "grafana_lb" {
   load_balancer_type = "application"
   security_groups    = ["sg-abc123abc"]
   subnets            = ["subnet-abcde012", "subnet-bcde012a"]
+  enable_deletion_protection = true
 }
 
 resource "aws_lb_target_group" "grafana_tg" {
   name     = "grafana-tg"
   port     = 3000
   protocol = "HTTP"
-  vpc_id   = "vpc-abc123abc"
+  vpc_id   = data.aws_vpc.placement.id
 
   health_check {
     healthy_threshold   = 3
@@ -26,10 +27,42 @@ resource "aws_lb_listener" "grafana_listener" {
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
-  certificate_arn   = data.aws_acm_certificate.cert.arn
+  certificate_arn   = local.ssl_certificate_arn
 
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.grafana_tg.arn
   }
+}
+
+resource "aws_acm_certificate" "certificate" {
+  count = local.create_ssl_certificate ? 1 : 0
+
+  domain_name               = "${var.service}.${var.environment}.${data.aws_route53_zone.selected.name}"
+  subject_alternative_names = ["*.${var.service}.${var.environment}.${data.aws_route53_zone.selected.name}"]
+  validation_method         = "DNS"
+}
+
+resource "aws_route53_record" "certificate_validation" {
+  for_each = local.create_ssl_certificate ? {
+    for dvo in aws_acm_certificate.certificate[0].domain_validation_options : dvo.domain_name => {
+      name    = dvo.resource_record_name
+      type    = dvo.resource_record_type
+      record  = dvo.resource_record_value
+    }
+  } : {}
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.selected.id
+}
+
+resource "aws_acm_certificate_validation" "certificate" {
+  count = local.create_ssl_certificate ? 1 : 0
+
+  certificate_arn         = aws_acm_certificate.certificate[0].arn
+  validation_record_fqdns = [for record in aws_route53_record.certificate_validation : record.fqdn]
 }
