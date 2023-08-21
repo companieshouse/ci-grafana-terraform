ff0fcf3c47152062cadd3ba2e996eb4b3d80db96
autmwatl <noreply@companieshouse.gov.uk>
2023-08-21 12:07:04 +0100

    DVOP-2545 add image version, cert name, repo name

--- groups/ecs/variables.tf
+++ groups/ecs/variables.tf
@@ -19,6 +19,12 @@ variable "repository_name" {
   type        = string
 }
 
+variable "image_repository_name" {
+  description = "The name of the repository that the container is built in"
+  default     = "ci-grafana-image"
+  type        = string
+}
+
 variable "service" {
   description = "The service name to be used when creating AWS resources"
   default     = "ci-grafana"
@@ -29,4 +35,15 @@ variable "team" {
   description = "The name of the team"
   default     = "platform"
   type        = string
+}
+
+variable "grafana_image_version" {
+  description = "The version of the ci-grafana-image container that we are using"
+  default = "latest"
+}
+
+variable "ssl_certificate_name" {
+  type        = string
+  description = "The name of an existing ACM certificate to use for the ELB SSL listener. Setting this disables certificate creation"
+  default = ""
 }
