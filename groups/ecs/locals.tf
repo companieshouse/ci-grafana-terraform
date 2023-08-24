a93974f8a89c4cf35e2edd119e86bb90991d8050
autmwatl <noreply@companieshouse.gov.uk>
2023-08-24 16:21:40 +0100

    DVOP-2545 requested changes

--- groups/ecs/locals.tf
+++ groups/ecs/locals.tf
@@ -18,10 +18,6 @@ locals {
   ssl_certificate_arn         = var.ssl_certificate_name == "" ? aws_acm_certificate_validation.certificate[0].certificate_arn : data.aws_acm_certificate.certificate[0].arn
 
   dns_zone_name               = local.secrets.dns_zone_name
-  load_balancer_dns_zone_name = local.secrets.load_balancer_dns_zone_name
-  image_owner_id              = local.secrets.image_owner_id
-  concourse_access_cidrs      = local.secrets.concourse_access_cidrs
-  web_access                  = concat(local.placement_subnet_cidrs, [local.concourse_access_cidrs])
   ecs_grafana_family          = "${var.service}-task"
 
 }
