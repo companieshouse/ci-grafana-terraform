67d16de46a477b240911ea06300366ac7759c2f2
autmwatl <noreply@companieshouse.gov.uk>
2023-08-25 11:04:41 +0100

    DVOP-2545 remove automation definitions

--- groups/ecs/locals.tf
+++ groups/ecs/locals.tf
@@ -11,8 +11,6 @@ locals {
   ))
   placement_subnet_pattern  = local.secrets.placement_subnet_pattern
   placement_vpc_pattern     = local.secrets.placement_vpc_pattern
-  automation_subnet_pattern = local.secrets.automation_subnet_pattern
-  automation_vpc_pattern    = local.secrets.automation_vpc_pattern
 
   create_ssl_certificate      = var.ssl_certificate_name == "" ? true : false
   ssl_certificate_arn         = var.ssl_certificate_name == "" ? aws_acm_certificate_validation.certificate[0].certificate_arn : data.aws_acm_certificate.certificate[0].arn
