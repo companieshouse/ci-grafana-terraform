2112243cae1fba7880da12d21b6619dbc084692f
autmwatl <noreply@companieshouse.gov.uk>
2023-09-05 18:47:07 +0100

    DVOP-2545 temporarily lb and service

--- groups/ecs/ecs.tf
+++ groups/ecs/ecs.tf
@@ -29,28 +29,28 @@ resource "aws_ecs_task_definition" "grafana_task" {
   )
 }
 
-resource "aws_ecs_service" "grafana_service" {
-  name                              = "${local.resource_prefix}-service"
-  cluster                           = aws_ecs_cluster.grafana_cluster.id
-  task_definition                   = aws_ecs_task_definition.grafana_task.arn
-  launch_type                       = var.ecs_grafana_launch_type
-  desired_count                     = var.grafana_service_desired_count
-  depends_on                        = [
-    aws_iam_role.ecs_execution_role,
-  ]
-
-  network_configuration {
-    subnets          = local.placement_subnet_ids
-    assign_public_ip = false
-    security_groups = [
-      aws_security_group.alb_security_group.arn,
-    ]
-  }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.grafana_tg.arn
-    container_name = "${local.resource_prefix}-container"
-    container_port = 3000
-  }
-
-}
+#resource "aws_ecs_service" "grafana_service" {
+#  name                              = "${local.resource_prefix}-service"
+#  cluster                           = aws_ecs_cluster.grafana_cluster.id
+#  task_definition                   = aws_ecs_task_definition.grafana_task.arn
+#  launch_type                       = var.ecs_grafana_launch_type
+#  desired_count                     = var.grafana_service_desired_count
+#  depends_on                        = [
+#    aws_iam_role.ecs_execution_role,
+#  ]
+#
+#  network_configuration {
+#    subnets          = local.placement_subnet_ids
+#    assign_public_ip = false
+#    security_groups = [
+#      aws_security_group.alb_security_group.arn,
+#    ]
+#  }
+#
+#  load_balancer {
+#    target_group_arn = aws_lb_target_group.grafana_tg.arn
+#    container_name = "${local.resource_prefix}-container"
+#    container_port = 3000
+#  }
+#
+#}
