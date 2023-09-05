4ecb85fbbddb16e13b77858e413f014129df652b
autmwatl <noreply@companieshouse.gov.uk>
2023-09-05 10:49:53 +0100

    DVOP-2545 additional perms

--- groups/ecs/iam.tf
+++ groups/ecs/iam.tf
@@ -12,6 +12,24 @@ data "aws_iam_policy_document" "ecs_execution_role_policy" {
   }
 }
 
+data "aws_iam_policy_document" "ecs_execution_permissions" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = [
+      "arn:aws:ecr:*:*:repository/${var.service}*",
+      "arn:aws:logs:*:*:log-group:${local.resource_prefix}*"
+    ]
+  }
+}
+
+
 data "aws_iam_policy_document" "ecs_task_role_policy" {
   statement {
     effect  = "Allow"
@@ -26,12 +44,46 @@ data "aws_iam_policy_document" "ecs_task_role_policy" {
   }
 }
 
+data "aws_iam_policy_document" "ecs_task_permissions" {
+  statement {
+    effect    = "Allow"
+    actions   = ["cloudwatch:ListMetrics"]
+    resources = ["arn:aws:cloudwatch:*:*:metric/${local.resource_prefix}*"]
+  }
+}
+
+
+resource "aws_iam_policy" "ecs_task_permissions" {
+  name        = "ECSTaskPermissions"
+  description = "Permissions for ECS Task Role"
+  policy      = data.aws_iam_policy_document.ecs_task_permissions.json
+}
+
+
+resource "aws_iam_policy" "ecs_execution_permissions" {
+  name        = "ECSExecutionPermissions"
+  description = "Permissions for ECS Execution Role"
+  policy      = data.aws_iam_policy_document.ecs_execution_permissions.json
+}
+
+
 resource "aws_iam_role" "ecs_execution_role" {
-  name = "ecs_execution_role"
+  name               = "${local.resource_prefix}-ecs_execution_role"
   assume_role_policy = data.aws_iam_policy_document.ecs_execution_role_policy.json
 }
 
 resource "aws_iam_role" "ecs_task_role" {
-  name = "ecs_task_role"
+  name               = "${local.resource_prefix}-ecs_task_role"
   assume_role_policy = data.aws_iam_policy_document.ecs_task_role_policy.json
+}
+
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_permissions_attach" {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn = aws_iam_policy.ecs_execution_permissions.arn
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_permissions_attach" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = aws_iam_policy.ecs_task_permissions.arn
 }
