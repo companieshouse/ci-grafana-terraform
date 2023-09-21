fcd8d57f264cd327f32d25839bf78bfbd72acb4c
autmwatl <noreply@companieshouse.gov.uk>
2023-09-21 10:03:03 +0100

    DVOP-2545 reduce healthy threshold

--- groups/ecs/lb.tf
+++ groups/ecs/lb.tf
@@ -15,10 +15,10 @@ resource "aws_lb_target_group" "grafana_tg" {
   target_type                = "ip"
 
   health_check {
-    healthy_threshold   = 10
+    healthy_threshold   = 3
     unhealthy_threshold = 10
     timeout             = 120
-    interval            = 300
+    interval            = 30
     path                = "/api/health"
     protocol            = "HTTP"
     port                = "3000"
