48ee08f641b7151b6d14c6012f383e5ac628c4f5
Wayne Jenkins <wjenkins1@companieshouse.gov.uk>
2023-11-13 14:09:04 +0000

    Add output for security group name and ID

--- groups/grafana/module-rds/outputs.tf
+++ groups/grafana/module-rds/outputs.tf
@@ -5,3 +5,11 @@ output "rds_endpoint" {
 output "rds_engine_version" {
   value = "${aws_db_instance.rds.engine}-${aws_db_instance.rds.engine_version_actual}"
 }
+
+output "rds_security_group_name" {
+  value = aws_security_group.rds.name
+}
+
+output "rds_security_group_id" {
+  value = aws_security_group.rds.id
+}
